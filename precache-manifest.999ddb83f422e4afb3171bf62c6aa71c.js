02c47c28330c5ce74b82705f389ee8cfc410f31f
Mat King <mat@matking.info>
2019-04-09 18:08:58 -0400

    Updates

---
+++ precache-manifest.999ddb83f422e4afb3171bf62c6aa71c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "e0abb8ffb152fd15d426",
+    "url": "/clickygame/static/js/main.89042371.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "e0abb8ffb152fd15d426",
+    "url": "/clickygame/static/css/main.6282ee65.chunk.css"
+  },
+  {
+    "revision": "2a0107f873a2656b59303aa4ec03427a",
+    "url": "/clickygame/index.html"
+  }
+];
